e5b554e7b4d646d725abcae84a696930e275456e
Rohan, Matkar <rohan.matkar489@gmail.com>
2019-02-12 12:04:28 +0530

    final

--- DynamoDB/newtemop.py
+++
@@ -1,6 +0,0 @@
-
-sentence1 = "this/is/a/temp/file"
-target = sentence1.split('/')
-temp = '-'.join(target)
-
-print(temp)
--- DynamoDB/test2.py
+++
@@ -1,5 +0,0 @@
-pagintor = s3.get_paginator('list_objects')
-pages = paginator.paginate(Bucket=bucket)
-for page in pages:
-    for obj in page['Contents']:
-        
